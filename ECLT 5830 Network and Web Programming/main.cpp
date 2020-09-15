9a92c6ae4ab374e70caf06201ed8a9ee90c8092c
ssw <250853210@qq.com>
2020-09-15 15:00:17 +0800

    Initial Commit

---
+++ ECLT 5830 Network and Web Programming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECLT 5830 Network and Web Programming
+//
+//  Created by 某某 s on 2020/9/15.
+//  Copyright © 2020 某某 s. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
